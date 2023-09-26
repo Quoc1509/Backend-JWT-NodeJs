e021bab26a99e1d9da4afb3c4973f88018e9b2bd
trunghuynh159 <trunghuynh159@gmail.com>
2023-09-26 02:30:58 -0700

    first-update

---
+++ src/routes/web.js
@@ -0,0 +1,18 @@
+import express, { Router } from 'express'
+
+const router = express.Router()
+
+
+/**
+ * 
+ * @param {*} app - express app 
+ */
+const initWebRoutes = (app) => {
+    router.get('/', (req, res) => {
+        return res.send("Hello Word")
+    })
+
+    return app.use('/', router)
+}
+
+export default initWebRoutes;
